3f4bd813fc30498a52e7839d52d1db54741269f0
Romain Coeur <coeurro@gmail.com>
2018-05-24 10:52:25 +0200

    add tag_set endpoint

--- README.md
+++ README.md
@@ -4,19 +4,64 @@ This is a stub for WCS student to run Android and React apps.
 The client for this stub is "Pompiers SDIS"
 format : json
 
-## Routes available :
+The API is a formal REST API and the following endpoints are avaiblable with no authentication.
+
+## Endpoints :
+
+### Groups
 
 * GET /groups
 * GET /groups/1
+* POST /groups
+* PUT /groups/1
+* PATCH /groups/1
+* DELETE /groups/1
+
+Required params for POST and PUT : `name`
+
+### Sessions
+
 * GET /sessions
 * GET /sessions/1
+* POST /sessions
+* PUT /sessions/1
+* PATCH /sessions/1
+* DELETE /sessions/1
+
+Required params for POST and PUT : `name`, `author`, `fk_group`, `video_link`
+
+### Tags
+
 * GET /tags
 * GET /tags/1
-* GET /session_tags
-* GET /session_tags/1
+* POST /tags
+* PUT /tags/1
+* PATCH /tags/1
+* DELETE /tags/1
 
-These routes return either a list or one element if the id is defined.
-You will be able to pass any id instead of 1 in the real API.
+Required params for POST and PUT : `name`, `tag_set`
+
+### Tag_sessions
+
+* GET /tag_sessions
+* GET /tag_sessions/1
+* POST /tag_sessions
+* PUT /tag_sessions/1
+* PATCH /tag_sessions/1
+* DELETE /tag_sessions/1
+
+Required params for POST and PUT : `fk_tag`, `fk_session`, `start`, `end`
+
+### Tag sets
+
+* GET /tag_sets
+* GET /tag_sets/1
+* POST /tag_sets
+* PUT /tag_sets/1
+* PATCH /tag_sets/1
+* DELETE /tag_sets/1
+
+Required params for POST and PUT : `name`
 
 ## Hosting
 
--- redirect.php
+++ redirect.php
@@ -4,83 +4,160 @@
 //echo $_SERVER['REQUEST_METHOD'];
 //echo $_GET['entity'];
 
+parse_str(file_get_contents("php://input"),$post_vars);
+
 // Details
-if (preg_match('#groups/1$#isU', $_SERVER['REDIRECT_URL'], $match) && $_SERVER['REQUEST_METHOD'] == "GET") {
-	header("Status:200 OK", false, 200);
-	require 'group1.json';
+if (preg_match('#groups/1$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+		header("Status: 200 OK", false, 200);
+		require 'group1.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($post_vars["name"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+		header("Status: 200 OK", false, 200);
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
+	}
 }
-elseif (preg_match('#tag_sessions/1$#isU', $_SERVER['REDIRECT_URL'], $match) && $_SERVER['REQUEST_METHOD'] == "GET") {
-        header("Status: 200 OK", false, 200);
-
-        require 'tag_session1.json';
+elseif (preg_match('#tag_sessions/1$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        	header("Status: 200 OK", false, 200);
+        	require 'tag_session1.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($post_vars["fk_tag"]) && isset($post_vars["fk_session"]) && isset($post_vars["start"]) && isset($post_vars["end"])) {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+                header("Status: 200 OK", false, 200);
+        }
+        else {
+                header("Status: 400 BAD REQUEST", false, 400);
+        }
 }
-elseif (preg_match('#sessions/1$#isU', $_SERVER['REDIRECT_URL'], $match) && $_SERVER['REQUEST_METHOD'] == "GET") {
-        header("Status: 200 OK", false, 200);
-
-        require 'session1.json';
+elseif (preg_match('#sessions/1$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        	header("Status: 200 OK", false, 200);
+        	require 'session1.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($post_vars["name"]) && isset($post_vars["author"]) && isset($post_vars["fk_group"]) && isset($post_vars["video_link"])) {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+                header("Status: 200 OK", false, 200);
+        }
+        else {
+                header("Status: 400 BAD REQUEST", false, 400);
+        }
 }
-elseif (preg_match('#tags/1$#isU', $_SERVER['REDIRECT_URL'], $match) && $_SERVER['REQUEST_METHOD'] == "GET") {
-        header("Status: 200 OK", false, 200);
-
-        require 'tag1.json';
+elseif (preg_match('#tags/1$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        	header("Status: 200 OK", false, 200);
+        	require 'tag1.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($post_vars["name"]) && isset($post_vars["fk_tag_set"])) {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+                header("Status: 200 OK", false, 200);
+        }
+        else {
+                header("Status: 400 BAD REQUEST", false, 400);
+        }
+}
+elseif (preg_match('#tag_sets/1$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+		header("Status: 200 OK", false, 200);
+		require 'tag_set1.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "PUT" && isset($post_vars["name"])) {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "PATCH") {
+                header("Status: 200 OK", false, 200);
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+                header("Status: 200 OK", false, 200);
+        }
+        else {
+                header("Status: 400 BAD REQUEST", false, 400);
+        }
 }
 
 
 // Lists
 elseif (preg_match('#groups$#isU', $_SERVER['REDIRECT_URL'], $match)) {
 	if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	        header("Status:200 OK", false, 200);
+	        header("Status: 200 OK", false, 200);
 	        require 'jgroups.json';
 	}
-	elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-		if (isset($_POST["name"])) {
-			header("Status:200 OK", false, 200);
-		}
-		else {
-			header("Status:400 BAD REQUEST", false, 400);
-		}
+	elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["name"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
 	}
 }
 elseif (preg_match('#tag_sessions$#isU', $_SERVER['REDIRECT_URL'], $match)) {
 	if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	        header("Status:200 OK", false, 200);
+	        header("Status: 200 OK", false, 200);
 	        require 'jtag_sessions.json';
 	}
-	elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-		if (isset($_POST["fk_tag"]) && isset($_POST["fk_session"]) && isset($_POST["start"]) && isset($_POST["end"])) {
-			header("Status:200 OK", false, 200);
-		}
-		else {
-			header("Status:400 BAD REQUEST", false, 400);
-		}
+	elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["fk_tag"]) && isset($_POST["fk_session"]) && isset($_POST["start"]) && isset($_POST["end"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
 	}
 }
 elseif (preg_match('#sessions$#isU', $_SERVER['REDIRECT_URL'], $match)) {
 	if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	        header("Status:200 OK", false, 200);
+	        header("Status: 200 OK", false, 200);
 	        require 'jsessions.json';
 	}
-	elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-		if (isset($_POST["name"]) && isset($_POST["author"]) && isset($_POST["fk_group"]) && isset($_POST["video_link"])) {
-			header("Status:200 OK", false, 200);
-		}
-		else {
-			header("Status:400 BAD REQUEST", false, 400);
-		}
+	elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["name"]) && isset($_POST["author"]) && isset($_POST["fk_group"]) && isset($_POST["video_link"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
 	}
 }
 elseif (preg_match('#tags$#isU', $_SERVER['REDIRECT_URL'], $match)) {
 	if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	        header("Status:200 OK", false, 200);
+	        header("Status: 200 OK", false, 200);
 	        require 'jtags.json';
 	}
-	elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-		if (isset($_POST["name"])) {
-			header("Status:200 OK", false, 200);
-		}
-		else {
-			header("Status:400 BAD REQUEST", false, 400);
-		}
+	elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["name"]) && isset($_POST["fk_tag_set"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
+	}
+}
+elseif (preg_match('#tag_sets$#isU', $_SERVER['REDIRECT_URL'], $match)) {
+	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+		header("Status: 200 OK", false, 200);
+		require 'jtag_sets.json';
+	}
+	elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["name"])) {
+		header("Status: 200 OK", false, 200);
+	}
+	else {
+		header("Status: 400 BAD REQUEST", false, 400);
 	}
 }
 
